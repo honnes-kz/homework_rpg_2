d350687674c3c040670be6e535978fd6df88c01d
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-02-19 02:05:03 +0500

    Factory methods

---
+++ src/com/narxoz/rpg/factory/IceComponentFactory.java
@@ -0,0 +1,22 @@
+package com.narxoz.rpg.factory;
+
+import java.util.List;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.combat.FrostBreath;
+import com.narxoz.rpg.combat.IceShield;
+import com.narxoz.rpg.loot.IceLootTable;
+import com.narxoz.rpg.loot.LootTable;
+
+public class IceComponentFactory implements EnemyComponentFactory{
+
+    public List<Ability> createAbilities(){
+        return List.of(new FrostBreath(),new IceShield());
+    }
+    public LootTable creatLootTable(){
+        return new IceLootTable();
+    }
+    public String createAIBehavior(){
+        return "Defensive";
+    }
+}
---
+++ src/com/narxoz/rpg/factory/ShadowComponentFactory.java
@@ -0,0 +1,22 @@
+package com.narxoz.rpg.factory;
+
+import java.util.List;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.combat.ShadowStrike;
+import com.narxoz.rpg.combat.Vanish;
+import com.narxoz.rpg.loot.LootTable;
+import com.narxoz.rpg.loot.ShadowLootTable;
+
+public class ShadowComponentFactory implements EnemyComponentFactory{
+
+    public List<Ability> createAbilities(){
+        return List.of(new ShadowStrike(),new Vanish());
+    }
+    public LootTable creatLootTable(){
+        return new ShadowLootTable();
+    }
+    public String createAIBehavior(){
+        return "Tactical";
+    }
+}
